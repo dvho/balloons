0a7543e72bc63dd622591319c6b6174c6e339d77
DH <dh@DHs-MacBook-Pro.local>
2019-01-02 16:23:54 -0500

    Testing mobile responsiveness.

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ const b50 = document.getElementById('balloon__50');
                     balloonNumber.style.zIndex = `-1`; //After the explosion recess the (now phantom) balloons because they seem occasionally to conflict with existing balloons.
                     pop.style.animation = ``; //Terminate explosion animation (precautionary).
                     pop.style.zIndex = `-1`; //After the explosion recess the (now phantom) explosions because they seem occasionally to conflict with existing balloons.
-                    pop.style.boxShadow = ``; //Make the shadow (i.e. the explosion) nonexistent since it will otherwise occasionally reappear on mobile Safari. 
+                    //pop.style.boxShadow = ``; //Make the shadow (i.e. the explosion) nonexistent since it will otherwise occasionally reappear on mobile Safari. 
                 }, balloonDiameter * .23);
             });
         })
