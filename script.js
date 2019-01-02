cd1b9147f2d42691d282c8c1cb36b67794784afa
DH <dh@DHs-MacBook-Pro.local>
2019-01-02 15:56:08 -0500

    Testing mobile responsiveness.

--- script.js
+++ script.js
@@ -75,20 +75,22 @@ const b50 = document.getElementById('balloon__50');
         let balloonNumber = eval(`b${i}`);
         [`mousedown`, `touchend`].forEach((e) => {
             balloonNumber.addEventListener(e, () => {
-                let balloonDiameter = parseInt(balloonNumber.style.width.split(`p`)[0]);
-                balloonNumber.style.animation = ``; //Terminate balloon animation.
-                explosionSwitcher = (explosionSwitcher + 1) % 2; //Alternates between 0 and 1 to switch between two identical animations.
-                score += 1; //Update score.
+                let balloonDiameter = parseInt(balloonNumber.style.width.split(`p`)[0]); //Get the balloon's diameter.
+                explosionSwitcher = (explosionSwitcher + 1) % 2; //Alternate between 0 and 1 to switch between two identical animations.
+                score += 1; //Update the score.
                 yourScore.textContent = score; //Display score.
-                pop.style.boxShadow = `0 0 ${balloonNumber.style.width} ${balloonNumber.style.height} ${balloonNumber.style.backgroundColor}, inset 0 0 ${balloonNumber.style.width} ${balloonNumber.style.height} ${balloonNumber.style.backgroundColor}`; //Note that a) balloon width and height are the same so I didn't necessarily need to refeerence both here, either one or the other would have worked, and b) those values (that value) being used in place of both blur and spread radii is not a mistake, it just so happens that the value dynamically works perfectly for blur and spread.
-                pop.style.left = `${x - balloonDiameter/2}px`;
-                pop.style.top = `${y - balloonDiameter/2}px`;
-                pop.style.zIndex = balloonNumber.style.zIndex;
-                pop.style.width = balloonNumber.style.width;
-                pop.style.height = balloonNumber.style.height;
-                pop.style.animation = `explosion${explosionSwitcher} ${balloonDiameter * .00023}s linear`;
+                pop.style.left = `${x - balloonDiameter/2}px`; //Position the explosion.
+                pop.style.top = `${y - balloonDiameter/2}px`; //Position the explosion.
+                pop.style.zIndex = balloonNumber.style.zIndex; //Set the zIndex of the explosion to that of the balloon.
+                pop.style.width = balloonNumber.style.width; //Set the dimentions of the explosion.
+                pop.style.height = balloonNumber.style.height; //Set the dimentions of the explosion.
+                pop.style.boxShadow = `0 0 ${balloonNumber.style.width} ${balloonNumber.style.height} ${balloonNumber.style.backgroundColor}, inset 0 0 ${balloonNumber.style.width} ${balloonNumber.style.height} ${balloonNumber.style.backgroundColor}`; //Final explosion position looks like this, note that a) balloon width and height are the same so I didn't necessarily need to refeerence both here, either one or the other would have worked, and b) those values (that value) being used in place of both blur and spread radii is not a mistake, it just so happens that the value dynamically works perfectly for blur and spread.
+                pop.style.animation = `explosion${explosionSwitcher} ${balloonDiameter * .00023}s linear`; //Explode for a duration commensurate with balloon diameter.
                 setTimeout(()=> {
+                    balloonNumber.style.animation = ``; //Terminate balloon animation.
+                    pop.style.animation = ``; //Terminate explosion animation (precautionary).
                     balloonNumber.style.zIndex = `-1`; //After the explosion recess the (now phantom) balloons because they seem occasionally to conflict with existing balloons.
+                    pop.style.zIndex = `-1`; //After the explosion recess the (now phantom) explosions because they seem occasionally to conflict with existing balloons.
                 }, balloonDiameter * .23);
             });
         })
